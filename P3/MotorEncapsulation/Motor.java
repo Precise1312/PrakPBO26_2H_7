e457bc79e16a3e745b9d460b24a98201ea5211cd
Nama Kamu <yayohajid@gmail.com>
2026-09-11 07:52:01 +0700

    P3_Percobaan1

---
+++ P3/MotorEncapsulation/Motor.java
@@ -0,0 +1,16 @@
+package P3.MotorEncapsulation;
+
+public class Motor {
+    public int kecepatan = 0;
+    public boolean kontakOn = false;
+
+    public void printStatus(){
+        if (kontakOn = true){
+            System.out.println("kontak on");
+        }
+        else{
+            System.out.println("kontak off");
+        }
+        System.out.println("kecepatan " + kecepatan + "\n");
+    }
+}
